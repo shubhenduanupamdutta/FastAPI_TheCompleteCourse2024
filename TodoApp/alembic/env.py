699da9ec6925e5f77ec354642ea74ecc4cf4a5cc
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-11-10 15:59:50 +0530

    Project 3 - Todo App - Alembic Configuration for Todo

--- TodoApp/alembic/env.py
+++ TodoApp/alembic/env.py
@@ -1,13 +1,17 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from config import settings
+from models import Base  # noqa: F401
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql+psycopg://{settings.db_user}:{settings.db_password}@{settings.db_host}:5432/{settings.db_name}",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +68,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,11 +1,11 @@
-import models
-from database import engine
+# import models
+# from database import engine
 from fastapi import FastAPI
 from routers import admin, auth, todo, users
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)  # Not needed after using Alembic
 # Will only run once and if only the database is not created
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentications"])
