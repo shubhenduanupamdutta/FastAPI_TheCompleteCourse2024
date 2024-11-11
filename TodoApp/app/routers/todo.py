5002e1682c0bd35dfea97abfafcdacd814c9d62e
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-11-11 21:09:57 +0530

    Project 4 - Todo App - Functional Redirect to Todo Page

--- TodoApp/app/routers/todo.py
+++ TodoApp/app/routers/todo.py
@@ -1,15 +1,47 @@
 from typing import Annotated, TypeAlias
 
-from fastapi import APIRouter, HTTPException, Path, status
+from fastapi import APIRouter, HTTPException, Path, Request, status
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 from ..database import DB_Dependency
 from ..models import Todo
-from ..oauth2 import UserDependency
+from ..oauth2 import UserDependency, get_current_user
 from ..schema import TodoRequest
+
+templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter()
 
 TodoId: TypeAlias = Annotated[int, Path(ge=1)]
+
+
+def redirect_to_login():
+    redirect_response = RedirectResponse(
+        url="/auth/login-page", status_code=status.HTTP_302_FOUND
+    )
+    redirect_response.delete_cookie(key="access_token")
+    return redirect_response
+
+
+### Pages ###
+
+
+@router.get("/todo-page")
+async def render_todo_page(request: Request, db: DB_Dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))  # type: ignore
+        if user is None:
+            return redirect_to_login()
+        todos = db.query(Todo).filter(Todo.owner_id == user.get("id")).all()
+        return templates.TemplateResponse(
+            "todo.html", {"request": request, "todos": todos}
+        )
+    except Exception as _exc:
+        return redirect_to_login()
+
+
+### Endpoints ###
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
