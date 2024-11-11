799c89fb8cad212a81a003c0813095fc2c5ccc3c
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-11-11 19:19:53 +0530

    Project 4 - Todo Full Stack - Serving Basic HTML

--- TodoApp/app/main.py
+++ TodoApp/app/main.py
@@ -1,12 +1,21 @@
 # import models
 # from database import engine
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
 from .routers import admin, auth, todo, users
 
 app = FastAPI()
 
 # models.Base.metadata.create_all(bind=engine)  # Not needed after using Alembic
 # Will only run once and if only the database is not created
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 @app.get("/healthy")
